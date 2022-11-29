d25502616ee78bb8259ee6df7734e6211139c27f
Trung Dang <dangngoctrung1212@gmail.com>
2022-11-29 22:53:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+class Validator {
+    constructor(options) {
+        this.formElement = document.querySelector(options.form)
+        this.rules = options.rules
+        this.perform()
+    }
+
+    // static method does not invoke constructor and no context
+    // validate by getting rule and selector form rules, set/unset innerText and add/remove classList to error <span>
+    validate(inputElement, rule) {
+        console.log(rule)
+        let errorMessage = rule.test(inputElement.value)
+        let errorElement = inputElement.parentElement.querySelector(".form-message")
+                  
+        if (errorMessage) {
+            errorElement.innerText = errorMessage
+            inputElement.parentElement.classList.add("invalid")
+        } else {
+            errorElement.innerText = ""
+            inputElement.parentElement.classList.remove("invalid")
+        }
+    }
+
+    // perform validation by loop through rules and selector
+    perform() {
+    if (this.formElement) {
+        this.rules.forEach((rule)=>{
+            let inputElement = this.formElement.querySelector(rule.selector)
+            if (inputElement) {
+                inputElement.onblur =() => {
+                    this.validate(inputElement, rule)
+                }
+            }
+        })
+    }    
+}
+
+//    Rules method
+   static isRequired(selector) {
+        return {
+            selector:selector,
+            test: function(value) {
+                return value.trim() ? undefined : 'This is required field'
+            }
+        }
+    }
+
+    static isEmail(selector) {
+        return {
+            selector:selector,
+            test: function(value) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value.trim())? undefined : "This must be a valid email address" 
+                
+            }
+        }    
+    }
+    static isPassword(selector) {
+        return {
+            selector:selector,
+            test: function(value) {
+                return value.trim().length >=8 ? undefined : "Password must be at least 8 characters"
+            }
+        }
+    }
+
+    static isConfPassword(selector, compareSelector) {
+        return {
+            selector : selector,
+            test: function(value) {
+                return value === document.querySelector(compareSelector).value? undefined : "Confirm password must be same as password"
+            }
+        } 
+    }
+}
