05b5448c22524d75a390e1b7713da8bee1986459
Trung Dang <dangngoctrung1212@gmail.com>
2022-11-30 22:47:43 +0700

    add focus event

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ class Validator {
     constructor(options) {
         this.formElement = document.querySelector(options.form)
         this.rules = options.rules
+        this.invalidClass = options.invalidClass
+        this.errorSelector = options.errorSelector
         this.perform()
     }
-
+    /* Todo: save rule become an array, validate each rule by loop through rules, break if errorMeasage   */
     // static method does not invoke constructor and no context
     // validate by getting rule and selector form rules, set/unset innerText and add/remove classList to error <span>
     validate(inputElement, rule) {
         console.log(rule)
         let errorMessage = rule.test(inputElement.value)
-        let errorElement = inputElement.parentElement.querySelector(".form-message")
+        let errorElement = inputElement.parentElement.querySelector(this.errorSelector)
                   
         if (errorMessage) {
             errorElement.innerText = errorMessage
-            inputElement.parentElement.classList.add("invalid")
+            inputElement.parentElement.classList.add(this.invalidClass)
         } else {
-            errorElement.innerText = ""
-            inputElement.parentElement.classList.remove("invalid")
+            this.removeInvalidClass(errorElement)
         }
     }
-
+    removeInvalidClass(errorElement) {
+        console.log(errorElement)
+        errorElement.parentElement.classList.remove(this.invalidClass)
+        errorElement.innerText= ""
+    }
     // perform validation by loop through rules and selector
     perform() {
     if (this.formElement) {
         this.rules.forEach((rule)=>{
             let inputElement = this.formElement.querySelector(rule.selector)
             if (inputElement) {
-                inputElement.onblur =() => {
-                    this.validate(inputElement, rule)
-                }
+                inputElement.addEventListener("blur", ()=>this.validate(inputElement,rule)) 
+                inputElement.onfocus = () => this.removeInvalidClass(inputElement.parentElement.querySelector(this.errorSelector))
             }
         })
     }    
@@ -63,7 +67,7 @@ class Validator {
         }
     }
 
-    static isConfPassword(selector, compareSelector) {
+    static isConfirmPassword(selector, compareSelector) {
         return {
             selector : selector,
             test: function(value) {
