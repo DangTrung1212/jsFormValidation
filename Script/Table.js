e5e3cc2a6d72fcc512397405a7f5bdfb0b172821
DangTrung1212 <dangngoctrung1212@gmail.com>
2022-12-09 12:54:40 +0700

    add table

---
+++ Script/Table.js
@@ -0,0 +1,24 @@
+// const table = new Table({
+//     data : data,
+//     parent_element :
+// })
+const data = [{
+    email : "trungdang@ga.co",
+    name: "trung dang"
+}]
+
+class Table {
+    constructor(option) {
+        this.data = option.data
+        this.parent_element = document.querySelector(option.parent_element)
+    }
+    checkValidData() {
+    }
+    renderTHead() {
+        if (this.data && this.data.length) {
+            const keys = Object.key(this.data[0])
+            return keys.
+        }        
+    }
+
+}
