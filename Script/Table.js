2bb7f9f14307d0bd671f3a7f74b746490f48a1fd
DangTrung1212 <dangngoctrung1212@gmail.com>
2022-12-13 22:33:21 +0700

    almost done table

--- Script/Table.js
+++ Script/Table.js
@@ -5,26 +5,48 @@
 const data = [{
     email : "trungdang@ga.co",
     name: "trung dang"
+},{
+    email : "trungdang@ga.co",
+    name: "trung dang"
 }]
-//
+const option = {
+    userData : data,
+    parentElement: ""
+}
 class Table {
     constructor(option) {
-        this.data = option.data
-        this.parent_element = document.querySelector(option.parent_element)
+        this.userData = option.userData
+        this.parentElement = document.querySelector(option.parentElement)
+        this.className = option.className
+        this.main()
     }
-
-    checkValidData() {
-    }
-
     renderTHead() {
-        if (this.data && this.data.length) {
-            const keys = Object.key(this.data[0])
-            return keys.map((key) => `<th>${key}</th>>`).join("")
+            const keys = Object.keys(this.userData[0])
+            return keys.map((key) => `<th>${key}</th>`).join("")
+    }
+    renderTBody() {
+        let tBody = ""
+        for (let i = 0; i < this.userData.length; i++) {
+            let row = Object.values(this.userData[i]).map((value)=>`<td>${value}</td>`).join("")
+            tBody = tBody.concat(`<tr>${row}</tr>`)
+        }
+        return tBody
+    }
+    main () {
+        if (this.userData && this.userData.length) {
+            this.parentElement.innerHTML = `
+            <table>
+                <thead>
+                ${this.renderTHead()}
+                </thead>
+                <tbody>
+                ${this.renderTBody()}
+                </tbody>
+            </table>
+            `
+            this.parentElement.querySelector("table").classList.add(this.className)
         }
     }
 }
-const table = new Table({
-    data:data
-    ,parent:""
-})
-console.log(table.renderTHead())
+
+
